80e4cc5a09f17b267f864665ef8a108b5598c6cf
Antonio Oliveira <antoniovini47@gmail.com>
2024-11-05 18:50:36 -0300

    Config details.

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { useState } from "react";
 function App() {
   const [counter, setCounter] = useState(0);
   return (
-    <div>
-      <button onClick={() => setCounter(counter + 1)}>{counter}</button>
-      <div className="App">Hello World</div>;
+    <div style={{ justifyContent: "center", alignContent: "center" }}>
+      <div className="App">Div Deleter</div>
+      <button onClick={() => setCounter(counter + 1)}>Counter: {counter}</button>
     </div>
   );
 }
